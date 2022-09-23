9081b8e7e5da3d5015bc54787d947e04e9d89f78
SpruceWareDev <jokere778@gmail.com>
2022-09-23 21:09:26 +0100

    Added TRUE and FALSE to symbol table
    TRUE and FALSE now exist.
    Also null value is now 'NULL' instead of just 'null'

--- basic.py
+++ basic.py
@@ -761,7 +761,9 @@ class Interpreter:
 ##############################
 
 global_symbol_table = SymbolTable()
-global_symbol_table.set("null", Number(0))
+global_symbol_table.set("NULL", Number(0))
+global_symbol_table.set("TRUE", Number(1))
+global_symbol_table.set("FALSE", Number(0))
 
 
 def run(fn, text):
